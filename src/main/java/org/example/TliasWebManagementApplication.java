6dc48b3179eefadce0a81a8f44e37fae53c8c653
Tong <1427275295@qq.com>
2026-06-10 02:12:28 +0800

    add token Filter

--- src/main/java/org/example/TliasWebManagementApplication.java
+++ src/main/java/org/example/TliasWebManagementApplication.java
@@ -2,7 +2,9 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
+@ServletComponentScan //开启了SpringBoot对Servlet组件的支持
 @SpringBootApplication
 public class TliasWebManagementApplication {
 
